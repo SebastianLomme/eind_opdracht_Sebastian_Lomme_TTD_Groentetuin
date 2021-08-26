f4c27d49f637a3e5ec3a32c095bc0143b187fb3b
Sebastian Lomme <sebastian.lomme@gmail.com>
2021-08-26 19:50:18 +0200

    functies gescreven zonder omgevingsfactoren

--- js/farm.js
+++ js/farm.js
@@ -1,6 +1,43 @@
+const corn = {
+    name: "corn",
+    yield: 30,
+    factors: {
+        sun: {
+            low: -50,
+            medium: 0,
+            high: 50,
+        },
+    },
+};
+
+const environmentFactors = {
+    sun: "low",
+};
+
+console.log(corn.yield)
+
 const getYieldForPlant = (str) => str;
 const getYieldForCrop = (str) => str;
 const getTotalYield = (str) => str;
 
+const getCostsForCrop = (kostenPerPlant, aantalPlanten) => kostenPerPlant * aantalPlanten;
+const getRevenueForCrop = (opbrengsPerplant, aantalPlanten) => opbrengsPerplant * aantalPlanten;
 
-module.exports = {getYieldForPlant, getYieldForCrop, getTotalYield};
+const getProfitForCrop = (opbrengsPerplant, aantalPlanten, cost) => (opbrengsPerplant * aantalPlanten) - cost
+
+const getTotalProfit = (crop1, crop2) => crop1 + crop2
+
+
+
+
+
+
+module.exports = {
+    getYieldForPlant,
+    getYieldForCrop,
+    getTotalYield,
+    getCostsForCrop,
+    getRevenueForCrop,
+    getProfitForCrop,
+    getTotalProfit,
+};
